46dabfdbc30ad94ef76c0c9291bf227154ed8857
ly <458423308@qq.com>
2020-10-31 08:57:56 +0800

    first commit

---
+++ happypet/src/pages/index/index.vue
@@ -0,0 +1,316 @@
+<template>
+  <view class="content">
+    <view class="top-bar">
+        <view class="top-bar-left">
+        <image src="../../static/img/user_icon.jpg"></image>
+        </view>
+
+        <view class="top-bar-center">
+
+        <image src="../../static/img/logo.jpg"></image>
+        </view>
+
+        <view class="top-bar-right">
+        <image src="../../static/img/zoom_glasses.jpg"></image>
+        </view>
+
+    </view>
+
+    <view class="friends">
+      <view class="friend-list">
+
+        <view class="friend-list-l">
+            <text class="tip"></text>
+              <view class="user-image">
+              <image src="../../static/img/user_list_icon.jpg"></image>
+              </view>
+       
+        </view>
+        <view class="friend-list-r">
+          <view class="friend-list-top">
+            <view class="friend-list-top-left">好友请求</view>
+            <view class="friend-list-top-right">11:34</view>
+          </view>
+
+          <view class="friend-list-bottom">在么？</view>
+        </view>
+
+        
+      </view>
+      
+
+      <view class="friend-list">
+
+        <view class="friend-list-l">
+            <text class="tip"></text>
+              <view class="user-image">
+              <image src="../../static/img/user_list_icon.jpg"></image>
+              </view>
+       
+        </view>
+        <view class="friend-list-r">
+          <view class="friend-list-top">
+            <view class="friend-list-top-left">好友请求</view>
+            <view class="friend-list-top-right">11:34</view>
+          </view>
+
+          <view class="friend-list-bottom">在么？</view>
+        </view>
+
+        
+      </view>
+
+      <view class="friend-list">
+
+        <view class="friend-list-l">
+            <text class="tip"></text>
+              <view class="user-image">
+              <image src="../../static/img/user_list_icon.jpg"></image>
+              </view>
+       
+        </view>
+        <view class="friend-list-r">
+          <view class="friend-list-top">
+            <view class="friend-list-top-left">好友请求</view>
+            <view class="friend-list-top-right">11:34</view>
+          </view>
+
+          <view class="friend-list-bottom">在么？</view>
+        </view>
+
+        
+      </view>
+
+      <view class="friend-list">
+
+        <view class="friend-list-l">
+            <text class="tip"></text>
+              <view class="user-image">
+              <image src="../../static/img/user_list_icon.jpg"></image>
+              </view>
+       
+        </view>
+        <view class="friend-list-r">
+          <view class="friend-list-top">
+            <view class="friend-list-top-left">好友请求</view>
+            <view class="friend-list-top-right">11:34</view>
+          </view>
+
+          <view class="friend-list-bottom">在么？</view>
+        </view>
+
+        
+      </view>
+
+      <view class="friend-list">
+
+        <view class="friend-list-l">
+            <text class="tip"></text>
+              <view class="user-image">
+              <image src="../../static/img/user_list_icon.jpg"></image>
+              </view>
+       
+        </view>
+        <view class="friend-list-r">
+          <view class="friend-list-top">
+            <view class="friend-list-top-left">好友请求</view>
+            <view class="friend-list-top-right">11:34</view>
+          </view>
+
+          <view class="friend-list-bottom">在么？</view>
+        </view>
+        <span class=""></span>
+
+        
+      </view>
+
+      <view class="friend-list">
+
+        <view class="friend-list-l">
+            <text class="tip"></text>
+              <view class="user-image">
+              <image src="../../static/img/user_list_icon.jpg"></image>
+              </view>
+       
+        </view>
+        <view class="friend-list-r">
+          <view class="friend-list-top">
+            <view class="friend-list-top-left">好友请求</view>
+            <view class="friend-list-top-right">11:34</view>
+          </view>
+
+          <view class="friend-list-bottom">在么？</view>
+        </view>
+
+        
+      </view>
+    </view>
+  </view>
+    
+    
+    
+    
+
+</template>
+
+<script>
+export default {
+    data(){
+        return{
+            title:'Hello'
+        }
+    },
+onLoad(){
+
+},
+methods:{
+
+}
+}
+
+</script>
+
+<style lang="scss" scope>
+
+  .content{
+    display:flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+    background: #ccc;
+
+  }
+
+  .top-bar{
+    height:90px;
+    width: 100%;
+    box-sizing: border-box;
+    display:flex;
+    flex-direction: row;
+    align-items: center;
+    justify-content: center;
+
+    background: white;
+    border-bottom:gray;
+     .top-bar-left{
+   
+        text-align: left;
+       height: 90px;/*元素高度*/
+       width: 33%;
+      float:left;
+      image{
+  
+        width:68px;
+        height:68px;
+        border-radius:16rpx;
+ 
+      }
+    }
+    .top-bar-center{
+
+      height: 88px;/*元素高度*/
+       width: 33%;
+      text-align: center;
+      float:center;
+      image{
+        
+        width:68px;
+        height:68px;
+        border-radius:16rpx;
+        
+      }
+    }
+    .top-bar-right{
+      height: 88px;
+      width: 33%;
+      float:right;
+
+       image{
+        width:68px;
+        height:68px;
+        float:right;
+
+        top:50%;
+        height:68px;
+        margin-top:-34px;
+        border-radius:16px;
+         
+        
+      }
+      
+    }
+  }
+  .main{
+
+  }
+  .friends{
+    width: 100%;
+
+    
+
+     .friend-list{
+    margin: 2px;
+    background: #fff;
+
+    box-sizing: border-box;
+    text-align:center;
+    width: 100%;
+    float:left;
+    .friend-list-l{
+      width: 20%;
+      height:140rpx;
+      float:left;
+
+         top:50%;
+        height:70px;
+        margin-top:70px;
+
+
+      image{
+        height: 78rpx;
+        width: 78rpx;
+        border-radius: 18px;
+      }
+    }
+    .friend-list-r{
+      flex-direction: column;
+      width: 80%;
+      height:140rpx;
+      float:right;
+
+      .friend-list-top{
+        width: 100%;
+        .friend-list-top-left{
+          float:left;
+          margin: 20rpx;
+          font-size: 20px;
+          color: grey;
+          width:35%;
+          
+
+          
+        }
+        .friend-list-top-right{
+          float:right;
+          margin: 20rpx;
+           font-size: 20px;
+          color: grey;
+          width:35%;
+
+
+        }
+      }
+
+      .friend-list-bottom{
+        
+      }
+
+    }
+  }
+    
+  }
+ 
+  
+ 
+ 
+</style>
---
+++ happypet/src/pages/index/login.vue
@@ -0,0 +1,103 @@
+<template>
+<view class="content">
+    <!--头部导航栏-->
+    <view class="top-bar">
+   
+    </view>
+    <view class="login-title">
+        密码帐号登录
+    </view>
+
+    <!--表单-->
+    <view class="app-form">
+        <input class="app-form-user" placeholder="请输入帐号" @input="onInput" />
+        <input class="app-form-password" placeholder="请输入密码" @input="onInput" />
+        <button class="app-form-loginbtn" type="warn" @click="submit()" plain>登录</button>
+    </view>
+    
+    <view class="login-way">
+        <span>帐号密码登陆</span>
+        <span>|</span>
+        <span>登陆遇到问题？</span>
+    </view>
+
+    <view>
+        <span>社交帐号登陆</span>
+    </view>
+     <view>
+        <span>注册即代表您同意《xxx社区协议》</span>
+    </view>
+
+</view>
+
+
+
+</template>
+
+<script>
+export default {
+    
+    data(){
+
+    },
+    onLoad(){
+console.log("create");
+    },
+    methods:{
+        submit(){
+            console.log("hello");
+            uni.navigateTo({
+                url:"index"
+            })
+        }
+    }
+}
+</script>
+
+
+<style lang="scss" scoped>
+span{
+    font-size: 0.6em;
+}
+  .content{
+      
+      display:flex;
+      flex-direction: column;
+      align-items: center;
+      
+  }
+  .top-bar{
+      height:90rpx;
+      width:100%;
+      box-sizing: border-box;
+      display: flex;
+      flex-direction: row;
+      align-items: center;
+      justify-content: center;
+
+      background: #FF0000;
+      border-bottom: grey;
+
+  }
+  .login-title{
+      margin: 50px;
+  }
+  .app-form{
+      height:300px;
+      width:70%;
+    .app-form-user{
+        margin: 50px;
+        border-bottom: thick double #aa0000;
+        
+    }
+    .app-form-password{
+        margin: 50px;
+        border-bottom: thick double #aa0000
+    }
+    .app-form-loginbtn{
+        margin: 50px;
+    }
+  }
+  
+
+</style>
